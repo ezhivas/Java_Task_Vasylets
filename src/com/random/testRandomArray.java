1188721e97592e03a0ba81356de8214cb287198b
Serhii Vasylets <akvatonk@gmail.com>
2020-10-21 15:22:57 +0300

    AT-1 array task updated

--- src/com/random/testRandomArray.java
+++ src/com/random/testRandomArray.java
@@ -1,6 +1,8 @@
 package com.random;
 
+import java.util.Arrays;
 import java.util.Random;
+
 
 public class testRandomArray {
 
@@ -8,22 +10,19 @@ public class testRandomArray {
 
         Random random = new Random();
         int[] sampleArray = new int[10];
-        System.out.println("Random array:");
 
         for (int i = 0; i < 10; i++) {
             sampleArray[i] = random.nextInt();
-            System.out.print(sampleArray[i] + " ");
+
         }
-        System.out.println();
+        System.out.println("Random array:");
+        System.out.println(Arrays.toString(sampleArray));
 
         for (int i = 0; i < 10; i++) {
             if (sampleArray[i] < 0) sampleArray[i] = 0;
         }
         System.out.println("Array without negative numbers: ");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print(sampleArray[i] + " ");
-        }
+        System.out.println(Arrays.toString(sampleArray));
     }
 }
 
