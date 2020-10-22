6fe19fac82dba10bf2c480d2289c2fb13bb22fcd
Serhii Vasylets <akvatonk@gmail.com>
2020-10-22 21:39:28 +0300

    AT-1 changes in testMathCLass

--- src/com/math/testMathClass.java
+++ src/com/math/testMathClass.java
@@ -10,10 +10,8 @@ public class testMathClass {
         System.out.print("Enter the first number : ");
         Scanner a = new Scanner(System.in);
         int x = a.nextInt();
-        Scanner b = new Scanner(System.in);
         System.out.print("Enter the second number : ");
-        int y = b.nextInt();
-        Scanner s = new Scanner(System.in);
+        int y = a.nextInt();
 
         while (true) {
             System.out.println(" ");
@@ -23,33 +21,26 @@ public class testMathClass {
             System.out.println("Choose 3 for MULTIPLICATION");
             System.out.println("Choose 4 for DIVISION");
             System.out.println("Choose 5 for EXIT");
-            int n = s.nextInt();
+            int n = a.nextInt();
             switch (n) {
                 case 1:
-                    int add;
-                    add = x + y;
-                    System.out.println("Result : " + add);
+                    System.out.println("Result : " + (x + y));
                     break;
 
                 case 2:
-                    int sub;
-                    sub = x - y;
-                    System.out.println("Result : " + sub);
+                    System.out.println("Result : " + (x - y));
                     break;
 
                 case 3:
-                    int mul;
-                    mul = x * y;
-                    System.out.println("Result : " + mul);
+                    System.out.println("Result : " + (x * y));
                     break;
 
                 case 4:
-                    float div;
-                    div = (float) x / y;
-                    System.out.print("Result : " + div);
+                    System.out.print("Result : " + (x / y));
                     break;
 
                 case 5:
+                    System.out.println("'Exit' option selected.");
                     System.exit(0);
             }
         }
